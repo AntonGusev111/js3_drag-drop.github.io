4db530648938b046d95afca31e71495ac983a0e9
GusevADresume <resumeGusevAD@gmail.com>
2023-01-09 00:24:52 +0300

    v1

---
+++ src/js/app.js
@@ -0,0 +1,159 @@
+import Column from "./column.js";
+
+class Listener{
+    constructor(){
+        this.card = undefined;
+        this.cardParent = undefined;
+        this.columns = document.querySelector('.column');
+        this.cards = document.querySelectorAll('.card')
+        this.table = {
+        todo: new Column(document.querySelector('.todo')),
+        inprogress: new Column(document.querySelector('.inprogress')),
+        done: new Column(document.querySelector('.done')),
+        };
+    };
+
+    drag(){
+        const main = document.querySelector(".table");
+        let elemBelow = "";
+        let lastCard ="";
+        let dragCardSize ="";
+
+        main.addEventListener("dragenter", (e) => {
+            if (e.target.classList.contains("column")) {
+            e.target.classList.add("drop");
+            }
+        });
+          
+        main.addEventListener("dragleave", (e) => {
+            if (e.target.classList.contains("drop")) {
+              e.target.classList.remove("drop");
+            }
+        });
+
+        main.addEventListener("dragstart", (e) => {
+        if (e.target.classList.contains("card")) {
+            e.dataTransfer.setData("text/plain", e.target.id);
+            dragCardSize = e.target.offsetHeight;
+            console.log(dragCardSize)
+            }
+          });
+
+        main.addEventListener("dragover", (e) => {
+            e.preventDefault();
+            elemBelow = e.target;
+            if (elemBelow.classList.contains("card")){
+                lastCard = e.target
+                elemBelow.style.marginTop = `${dragCardSize+10}px`;
+            };
+
+        });
+
+        main.addEventListener("drop", (e) => {
+            const card = main.querySelector(`[id="${e.dataTransfer.getData("text/plain")}"]`);
+            if (elemBelow === card) {
+                return;
+            };
+            if (elemBelow.classList.contains("card")) {
+                const center =elemBelow.getBoundingClientRect().y + elemBelow.getBoundingClientRect().height / 2;
+                if (e.clientY > center) {
+                    if (elemBelow.nextElementSibling !== null) {
+                        elemBelow = elemBelow.nextElementSibling;
+                    } else{
+                        return;
+                    }
+                }
+                elemBelow.parentElement.insertBefore(card, elemBelow);
+            }
+            if (e.target.classList.contains("column")) {
+                if (lastCard && elemBelow.classList[1]==lastCard.parentElement.classList[1]){
+                    lastCard.parentElement.insertBefore(card, lastCard);
+                    card.style.marginTop = '20px';
+                    lastCard.style.marginTop = '20px';
+                    this.saveState();
+                    this.cleanState();
+                    return;
+                }
+                e.target.append(card);
+            }
+            this.saveState();
+            this.cleanState();
+        });
+        
+
+    }
+
+    listener(){
+        this.drag();
+        document.addEventListener('click',(e)=>{
+            if(e.target.className == 'footer'){
+                this.table[e.target.parentElement.classList[1]].showAddBlock()
+            };
+            if(e.target.className == 'cross'){
+                const parent = e.target.parentElement.parentElement.classList[1];
+                this.table[parent].delCard(e.target.parentElement)
+            };
+        });
+        document.addEventListener("mouseover", (e)=>{  
+            if(e.target.classList[0] =='card'){ 
+                let cross = e.target.querySelector('.cross')
+                cross.classList.remove('fog');
+                cross ='';
+            };
+        });
+        document.addEventListener("mouseout", (e)=>{
+            if(e.target.classList[0] =='card'){
+                let cross = e.target.querySelector('.cross')
+                setTimeout(()=>{
+                    cross.classList.add('fog');
+                    cross ='';
+                }, 1200);
+           };
+        });
+
+    };
+
+    localStateRead(){
+        if(localStorage.length == 0){
+           return
+        } else {
+            for (let key in localStorage){
+                if(localStorage.getItem(key) != null){
+                    this.restoreState(key,localStorage.getItem(key))
+                }
+            }
+        }
+    };
+
+    restoreState(key,value){
+        const values = JSON.parse(value)
+        for (let jkey in values){
+            this.table[key].createCard(jkey, values[jkey])
+        }
+    };
+
+    cleanState(){
+        const allCards = document.querySelectorAll('.card');
+        for (let card of allCards){
+            card.remove()
+        };
+        this.localStateRead()
+    };
+
+    saveState(){
+        for (let column of Object.keys(this.table)){
+            this.table[column].saveState();
+        };
+    };
+
+};
+
+
+
+
+const listener = new Listener();
+
+
+listener.localStateRead();
+listener.listener();
+
